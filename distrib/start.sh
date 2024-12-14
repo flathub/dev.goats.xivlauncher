8657665fe19401d2d72d4dd48bffc33aa51ce663
Blooym <19539165+Blooym@users.noreply.github.com>
2024-12-14 17:14:48 +0000

    fix start cmd

--- distrib/start.sh
+++ distrib/start.sh
@@ -1,1 +1,1 @@
-xlm launch --run-as-steam-compat-tool=false --xlcore-release-asset XIVLauncher.Core-flatpak.tar.gz --$@
+xlm launch --run-as-steam-compat-tool=false --xlcore-release-asset XIVLauncher.Core-flatpak.tar.gz $@
